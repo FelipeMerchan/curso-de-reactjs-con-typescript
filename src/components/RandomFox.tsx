e5a27d9c09242842a97e892d612e4c4c76d4f86f
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-07-13 18:37:12 -0500

    9. Tipos para referencias y observadores

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+
 /* En este archivo mostramos 4 formas (hay más) de crear
 un componetne de React en TypeScrip */
 /* #1 Tipado implícito
@@ -9,11 +11,16 @@ type Props = { image: string };
 
 /* #2 Tipar el retorno de la función */
 export const RandomFox = ({ image }: Props): JSX.Element => {
+  /* Debemos tipar useRef utilizando el genérico e indicarle
+  cuál es el tipo de elemento en el dom que vamos a usar con useRef: */
+  const node = useRef<HTMLImageElement>(null);
+
   return (
     // eslint-disable-next-line @next/next/no-img-element
     <img
       className="rounded"
       src={image}
+      ref={node}
       alt="Image for a Fox"
       width="320"
       height="auto"
