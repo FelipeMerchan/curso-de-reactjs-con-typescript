00b06defdbcbb94a0a5283d939549260c4cdd053
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-07-13 01:15:37 -0500

    7. State con tipos personalizados

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -6,19 +6,24 @@ import { RandomFox } from "@/components/RandomFox";
 /* Generate a random function between 1 and 123 */
 const random = () => Math.floor(Math.random() * 123) + 1;
 
+/* Generate simple unique id */
+const generateId = () => Math.random().toString(36).substr(2, 9);
+
+type ImageItem = { id: string; url: string };
+
 export const ImageList = (): JSX.Element => {
-  const [images, setImages] = useState<string[]>([
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
+  const [images, setImages] = useState<Array<ImageItem>>([
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
   ]);
 
   return (
     <>
-      {images.map((image, index) => (
-        <div key={index} className="p-4">
-          <RandomFox image={image} />
+      {images.map(({ id, url }) => (
+        <div key={id} className="p-4">
+          <RandomFox image={url} />
         </div>
       ))}
     </>
